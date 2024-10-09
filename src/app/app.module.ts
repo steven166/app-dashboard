b692856acba0ac351f4329b2c1a4d615d29b6a46
Rob Sonke <r.sonke@maxxton.com>
2024-10-09 21:20:28 +0200

    Updating to all latest deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
-import { SocialLoginModule, SocialAuthServiceConfig, DummyLoginProvider, SocialUser } from 'angularx-social-login';
-import { GoogleLoginProvider } from 'angularx-social-login';
 import { AppComponent } from './app.component';
 import { CallbackComponent } from './callback/callback.component';
 import { HomeComponent } from './home/home.component';
@@ -13,54 +11,54 @@ import { ConfigService } from './config/config.service';
 
 
 
-function getSocialConfig(configService: ConfigService): Promise<SocialAuthServiceConfig> {
-  const googleConfig: Promise<SocialAuthServiceConfig> = configService.getConfig().then(config => {
-    if (config.oauthProvider && config.oauthProvider === 'Google') {
-      return {
-        autoLogin: true,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              config.google.clientId,
-              {
-                'apiKey': config.google.clientSecret,
-                'clientId': config.google.clientId,
-                'scope': 'email',
-                'hosted_domain': config.google.hosted_domain,
-                'discoveryDocs': ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
-              }
-            )
-          }
-        ]
-      };
-    }
-    // otherwise return the dummy provider, to avoid errors
-    const dummyUser: SocialUser = {
-      id: '0123456789',
-      name: 'Dummy User',
-      email: 'info@maxxton.com',
-      firstName: 'Dummy',
-      lastName: 'User',
-      authToken: 'dummyAuthToken',
-      photoUrl: '',
-      provider: 'DUMMY',
-      idToken: 'dummyIdToken',
-      authorizationCode: 'dummyAuthCode',
-      response: null
-    };
-    return {
-      autoLogin: true,
-      providers: [
-        {
-          id: DummyLoginProvider.PROVIDER_ID,
-          provider: new DummyLoginProvider(dummyUser)
-        }
-      ]
-    };
-  });
-  return googleConfig;
-}
+// function getSocialConfig(configService: ConfigService): Promise<SocialAuthServiceConfig> {
+//   const googleConfig: Promise<SocialAuthServiceConfig> = configService.getConfig().then(config => {
+//     if (config.oauthProvider && config.oauthProvider === 'Google') {
+//       return {
+//         autoLogin: true,
+//         providers: [
+//           {
+//             id: GoogleLoginProvider.PROVIDER_ID,
+//             provider: new GoogleLoginProvider(
+//               config.google.clientId,
+//               {
+//                 'apiKey': config.google.clientSecret,
+//                 'clientId': config.google.clientId,
+//                 'scope': 'email',
+//                 'hosted_domain': config.google.hosted_domain,
+//                 'discoveryDocs': ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
+//               }
+//             )
+//           }
+//         ]
+//       };
+//     }
+//     // otherwise return the dummy provider, to avoid errors
+//     const dummyUser: SocialUser = {
+//       id: '0123456789',
+//       name: 'Dummy User',
+//       email: 'info@maxxton.com',
+//       firstName: 'Dummy',
+//       lastName: 'User',
+//       authToken: 'dummyAuthToken',
+//       photoUrl: '',
+//       provider: 'DUMMY',
+//       idToken: 'dummyIdToken',
+//       authorizationCode: 'dummyAuthCode',
+//       response: null
+//     };
+//     return {
+//       autoLogin: true,
+//       providers: [
+//         {
+//           id: DummyLoginProvider.PROVIDER_ID,
+//           provider: new DummyLoginProvider(dummyUser)
+//         }
+//       ]
+//     };
+//   });
+//   return googleConfig;
+// }
 
 export function loadAuthService(authService: AuthService): Function {
   return () => authService.load();
@@ -76,16 +74,16 @@ export function loadAuthService(authService: AuthService): Function {
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(ROUTES, { relativeLinkResolution: 'legacy' }),
-    SocialLoginModule
+    // SocialLoginModule
   ],
   providers: [
     AuthService,
     ConfigService,
-    {
-      provide: 'SocialAuthServiceConfig',
-      useFactory: getSocialConfig,
-      deps: [ ConfigService ]
-    },
+    // {
+    //   provide: 'SocialAuthServiceConfig',
+    //   useFactory: getSocialConfig,
+    //   deps: [ ConfigService ]
+    // },
     { provide: APP_INITIALIZER, useFactory: loadAuthService , deps: [ AuthService ], multi: true }
   ],
   bootstrap: [AppComponent]
